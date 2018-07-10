c04e385f4fc96b68e14054001f854f78af6f0802
blairly <blair.harper@gmail.com>
2018-07-10 15:11:05 +0100

    Implemented styling for homepage.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import LoginControl from './components/logincontrol/LoginControl';
 import Navigation from './components/navbar/Navigation';
 import MainCarousel from './components/carousel/MainCarousel';
+import HomeBody from './components/homebody/HomeBody';
 
 class App extends Component {
   render() {
@@ -14,10 +15,14 @@ class App extends Component {
           <title>Tom Francombe Music</title>
          
         </header>
-        
-        <Navigation />
+        <div id = "navbar">
+          <Navigation />
+        </div>
         <div className="carousel-container">
           <MainCarousel />
+        </div>
+        <div id="body-container">
+          <HomeBody />
         </div>
       </div>
     );
